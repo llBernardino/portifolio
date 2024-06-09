b256659986f1be5ea247058892680725b08c953e
Caua <cauaberrnardinolima@gmail.com>
2024-06-09 19:22:05 -0300

    adjustes

--- src/components/pages/index/Hero/index.tsx
+++ src/components/pages/index/Hero/index.tsx
@@ -22,10 +22,10 @@ export const Hero = (props: HeroTypes) => {
                 <Subtext>Experiência comprovada na construção de produtos de sucesso para clientes de todo o Brasil.</Subtext>
               </SubBox>
               <Contacts className='mt-4'>
-                <a className='mx-2 ' href="https://www.linkedin.com/in/cauã-bernardino-lima-73413b22a/" rel='noreferrer' target="_blank"><img alt='LinkedIn' src="https://img.shields.io/badge/-LinkedIn-%230077B5?style=for-the-badge&logo=linkedin&logoColor=white" /></a>
-                <a className='mx-2 ' href="https://github.com/llBernardino" target="_blank" rel='noreferrer'><img alt='github' src="https://img.shields.io/badge/Github-black?style=for-the-badge&logo=github&logoColor=white" /></a>
-                <a className='mx-2 ' href = "mailto:cauaberrnardinolima@gmail.com" target="_blank" rel='noreferrer' ><img alt='Gmail' src="https://img.shields.io/badge/Gmail-white?style=for-the-badge&logo=gmail&logoColor=red" /></a>
-                <a className='mx-2 ' href="https://contate.me/devcaua" target="_blank" rel='noreferrer'><img alt='whatsapp' src="https://img.shields.io/badge/WhatsApp-25D366?style=for-the-badge&logo=whatsapp&logoColor=white" /></a>
+                <a className='mx-2' href="https://www.linkedin.com/in/cauã-bernardino-lima-73413b22a/" rel='noreferrer' target="_blank"><img  className='mt-2' alt='LinkedIn' src="https://img.shields.io/badge/-LinkedIn-%230077B5?style=for-the-badge&logo=linkedin&logoColor=white" /></a>
+                <a className='mx-2' href="https://github.com/llBernardino" target="_blank" rel='noreferrer'><img  className='mt-2' alt='github' src="https://img.shields.io/badge/Github-black?style=for-the-badge&logo=github&logoColor=white" /></a>
+                <a className='mx-2' href = "mailto:cauaberrnardinolima@gmail.com" target="_blank" rel='noreferrer' ><img  className='mt-2' alt='Gmail' src="https://img.shields.io/badge/Gmail-white?style=for-the-badge&logo=gmail&logoColor=red" /></a>
+                <a className='mx-2' href="https://contate.me/devcaua" target="_blank" rel='noreferrer'><img  className='mt-2' alt='whatsapp' src="https://img.shields.io/badge/WhatsApp-25D366?style=for-the-badge&logo=whatsapp&logoColor=white" /></a>
               </Contacts>
             </Boxtext>
             <Figure className='select' data-hidden-class="hidden"><Img  src={props.perfil} alt='PERFIL' /></Figure>
